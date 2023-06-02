a2a7f676d6de27d21e98b4d3faed2b3b21fb6b7e
Toa Carney <toa@toacarney.nz>
2023-06-02 22:54:16 +1200

    add nav info

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -2,6 +2,10 @@
     <div class="p-2 pb-0">
         <x-card>
             <h1 class="text-4xl text-center">Todos</h1>
+            <div class="flex justify-between">
+                <span>{{ auth()->user()->name . " - " . auth()->user()->email }}</span>
+                <a class="border underline rounded-md p-1" href="{{ route('auth.logout') }}">Log Out</a>
+            </div>
         </x-card>
     </div>
 
