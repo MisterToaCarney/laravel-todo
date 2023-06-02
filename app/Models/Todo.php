9946364e995dff86f49360681917345df1ae85e8
Toa Carney <toa@toacarney.nz>
2023-06-02 22:22:48 +1200

    users have many todos

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -5,9 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Todo extends Model
 {
     use HasFactory;
 
     protected $fillable = ["name"];
+
+    /**
+     * Get the user that owns the Todo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
